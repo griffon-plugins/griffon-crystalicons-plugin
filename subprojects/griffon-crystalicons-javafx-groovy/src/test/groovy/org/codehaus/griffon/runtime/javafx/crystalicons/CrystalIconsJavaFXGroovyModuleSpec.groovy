277d62f6aa3e60f4277c6ce13f1b48cc2d3f8513
aalmiray <aalmiray@gmail.com>
2014-08-05 17:02:34 +0200

    First commit

---
+++ subprojects/griffon-crystalicons-javafx-groovy/src/test/groovy/org/codehaus/griffon/runtime/javafx/crystalicons/CrystalIconsJavaFXGroovyModuleSpec.groovy
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agratcow to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.griffon.runtime.javafx.crystalicons
+
+import griffon.builder.javafx.factory.CrystalIconFactory
+import griffon.core.GriffonApplication
+import griffon.core.test.GriffonUnitRule
+import griffon.javafx.support.crystalicons.CrystalIcon
+import griffon.plugins.crystalicons.Crystal
+import griffon.util.BuilderCustomizer
+import griffon.util.CompositeBuilder
+import javafx.embed.swing.JFXPanel
+import org.junit.Rule
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import javax.annotation.Nonnull
+import javax.inject.Inject
+
+import static griffon.util.AnnotationUtils.sortByDependencies
+
+/**
+ * @author Andres Almiray
+ */
+class CrystalIconsJavaFXGroovyModuleSpec extends Specification {
+    static {
+        new JFXPanel()
+    }
+
+    @Rule
+    public final GriffonUnitRule griffon = new GriffonUnitRule()
+
+    @Inject
+    private GriffonApplication application
+
+    @Shared
+    private Crystal trash = new Crystal('actions', 'trash')
+
+    def 'Builder customizer is configured correctly'() {
+        when:
+        FactoryBuilderSupport builder = createBuilder(application)
+
+        then:
+        builder.factories.containsKey('crystalIcon')
+        builder.factories.crystalIcon.class == CrystalIconFactory
+    }
+
+    @Unroll
+    def 'Can create a CrystalIcon using the factory'() {
+        given:
+        FactoryBuilderSupport builder = createBuilder(application)
+
+        when:
+        CrystalIcon icon = builder.crystalIcon(value, icon: crystal)
+
+        then:
+        icon.crystal == expected
+
+        where:
+        value                                      | crystal                                    || expected
+        Crystal.findByDescription('actions:trash') | null                                       || trash
+        null                                       | Crystal.findByDescription('actions:trash') || trash
+        null                                       | 'ACTIONS:TRASH'                            || trash
+    }
+
+    private static final String BUILDER_CUSTOMIZER = 'BuilderCustomizer'
+
+    @Nonnull
+    protected FactoryBuilderSupport createBuilder(@Nonnull GriffonApplication application) {
+        Collection<BuilderCustomizer> customizers = resolveBuilderCustomizers(application)
+        return new CompositeBuilder(customizers.toArray(new BuilderCustomizer[customizers.size()]))
+    }
+
+    @Nonnull
+    private Collection<BuilderCustomizer> resolveBuilderCustomizers(@Nonnull GriffonApplication application) {
+        Collection<BuilderCustomizer> customizerInstances = application.injector.getInstances(BuilderCustomizer)
+        return sortByDependencies(customizerInstances, BUILDER_CUSTOMIZER, 'customizer').values()
+    }
+}
---
+++ subprojects/griffon-crystalicons-swing/src/main/java/griffon/swing/support/crystalicons/CrystalIcon.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agratcow to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package griffon.swing.support.crystalicons;
+
+import griffon.plugins.crystalicons.Crystal;
+
+import javax.annotation.Nonnull;
+import javax.swing.ImageIcon;
+import java.net.URL;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * @author Andres Almiray
+ */
+public class CrystalIcon extends ImageIcon {
+    private final Crystal crystal;
+    private final int size;
+
+    public CrystalIcon(@Nonnull Crystal crystal) {
+        this(crystal, 16);
+    }
+
+    public CrystalIcon(@Nonnull Crystal crystal, int size) {
+        super(toURL(crystal, size));
+        this.crystal = crystal;
+        this.size = size;
+    }
+
+    public CrystalIcon(@Nonnull String description) {
+        this(Crystal.findByDescription(description));
+    }
+
+    @Nonnull
+    private static URL toURL(@Nonnull Crystal crystal, int size) {
+        requireNonNull(crystal, "Argument 'crystal' must not be null.");
+        String resource = crystal.asResource(size);
+        URL url = Thread.currentThread().getContextClassLoader().getResource(resource);
+        if (url == null) {
+            throw new IllegalArgumentException("Icon " + crystal.formatted() + " does not exist");
+        }
+        return url;
+    }
+
+    @Nonnull
+    public Crystal getCrystal() {
+        return crystal;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
